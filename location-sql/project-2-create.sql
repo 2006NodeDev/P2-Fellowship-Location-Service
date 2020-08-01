fd6434a6201af3c25f4aa1e34a0ee590ba747fcd
Your Name <you@example.com>
2020-08-01 19:14:51 -0400

    location sql script

---
+++ location-sql/project-2-create.sql
@@ -0,0 +1,54 @@
+create schema project_2;
+set schema 'project_2';
+
+--drop table users;
+--drop table locations;
+--drop table users_locations;
+--drop table location_images;
+--drop table locations_location_images;
+
+create table users(
+	"user_id" serial primary key,
+	"username" text not null unique,
+	"password" text not null,
+	"first_name" text not null,
+	"last_name" text,
+	"affiliation" text not null,
+	"places_visited" int, 
+	"address" text,
+	"email" text not null,
+	"role" text not null,
+	"image" text
+);
+
+create table locations(
+	"location_id" serial primary key,
+	"name" text not null unique,
+	--"image"	text,
+	"realm" text not null,
+	"governance" text,
+	"primary_population" text,
+	"description" text,
+	"avg_rating" int, 
+	"num_visited" int
+);
+	
+create table users_locations(
+	"user_id" int references users ("user_id"),
+	"location_id" int references locations ("location_id"),
+	"rating" int,
+	--"comment" text,
+	primary key ("user_id", "location_id")
+);
+
+create table location_images(
+	"image_id" serial primary key,
+	"image" text not null
+);
+
+
+create table locations_location_images(
+	"location_id" int references locations("location_id"),
+	"image_id" int references location_images("image_id"),
+	primary key ("location_id", "image_id")
+);
