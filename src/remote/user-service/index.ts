1d9a7d89c8be885b8a945b3507a2c456ff3fbac4
Arlette Inserto <you@example.com>
2020-08-09 02:34:31 -0700

    added user service host to baseUrl

--- src/remote/user-service/index.ts
+++ src/remote/user-service/index.ts
@@ -2,7 +2,7 @@
  
 
 //an optional env for host address or localhost default
-let baseURL = process.env['http://localhost:2007'] // || LB_USER_SERVICE_HOST'
+let baseURL = process.env['P2_USER_SERVICE_HOST'] || 'http://localhost:2007'
 
 
 export const userServiceBaseClient = axios.create({
